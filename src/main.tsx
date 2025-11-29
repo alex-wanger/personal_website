062d54bcb21ecb320f0bc4dc59e8b3804eb0cbf5
Alex Wang <alexwang94834@gmail.com>
2025-11-29 16:19:34 -0500

    add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "./index.css"
 import Portfolio from './pages/Portfolio.tsx';
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-      <Portfolio/>
-  </StrictMode>,
+    <StrictMode>
+        <Portfolio />
+        <Analytics />
+    </StrictMode>,
 )
